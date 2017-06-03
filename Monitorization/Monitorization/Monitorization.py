829149c7b7fc8ead0e32d4792c878b638006ca7a
Gustavo Vaccaro <fabianvaccaro@hotmail.com>
2017-06-03 23:00:23 +0200

    25

--- Monitorization/Monitorization/Monitorization.py
+++ Monitorization/Monitorization/Monitorization.py
@@ -2,6 +2,6 @@ import picamera
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
-    camera.start_recording('my_video.h264', format = 'h264', quality=35)
+    camera.start_recording('my_video.h264', format = 'h264', quality=25)
     camera.wait_recording(10)
     camera.stop_recording()
