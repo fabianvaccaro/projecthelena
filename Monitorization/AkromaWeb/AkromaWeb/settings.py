786793e63f0623be6d9199fecc93dae99d7dc301
Gustavo Vaccaro <fabianvaccaro@hotmail.com>
2017-06-03 23:26:11 +0200

    allowed hosts

--- Monitorization/AkromaWeb/AkromaWeb/settings.py
+++ Monitorization/AkromaWeb/AkromaWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '55706490-5aff-41c5-8480-aeab666b7b50'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
